ece666e1ccf803b7a8030884a9ce919effe30b0d
karthik1720 <karthik.cs1720@gmail.com>
2022-12-05 22:11:37 +0530

    Carousal - inprogress

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-
+  name: string = "karthik";
+  images: any[] = ["banner1.jpg", "banner2.jpg", "banner3.jpeg"];
+  i: any = 0;
+  newimg: any = this.images[0];
+  changeImg() {
+    if (this.i < this.images.length - 1) {
+      this.i++;
+    } else {
+      this.i = 0;
+    }
+    this.newimg = this.images[this.i];
+  }
 }
