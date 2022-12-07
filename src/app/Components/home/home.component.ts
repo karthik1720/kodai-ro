4eeda38808aa24b48d561b1748ac21ab0fe93e6c
karthik1720 <karthik.cs1720@gmail.com>
2022-12-08 00:08:31 +0530

    Scroll To Top - inprogress

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,21 +1,48 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.changeImg();
+    window.addEventListener("scroll", (e) => {
+      this.onScroll();
+    });
+  }
+  posCurrent: number = 0;
   name: string = "karthik";
   images: any[] = ["banner1.jpg", "banner2.jpg", "banner3.jpeg"];
   i: any = 0;
+  showIcon: boolean = false;
+  notShowIcon: boolean = true;
   newimg: any = this.images[0];
-  changeImg() {
-    if (this.i < this.images.length - 1) {
-      this.i++;
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+  onScroll() {
+    if (window.scrollY < this.posCurrent) {
+      this.showIcon = true;
+      this.notShowIcon = false;
     } else {
-      this.i = 0;
+      this.showIcon = false;
+      this.notShowIcon = true;
     }
-    this.newimg = this.images[this.i];
+    this.posCurrent = window.scrollY;
+  }
+
+  changeImg() {
+    setInterval(() => {
+      if (this.i < this.images.length - 1) {
+        this.i++;
+      } else {
+        this.i = 0;
+      }
+      this.newimg = this.images[this.i];
+    }, 3000);
   }
 }
